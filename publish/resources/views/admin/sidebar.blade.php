422209857018faf9c7db8170448c5aa9989e5901
Fishmad <fishmadwebdesign@gmail.com>
2018-01-14 15:59:14 +1100

    Initial commit

---
+++ publish/resources/views/admin/sidebar.blade.php
@@ -0,0 +1,17 @@
+<div class="col-md-3">
+            <div class="panel panel-default panel-flush">
+                <div class="panel-heading">
+                  Generator
+                </div>
+
+                <div class="panel-body">
+                    <ul class="nav" role="tablist">
+                            <li role="presentation">
+                                <a href="/generator">
+                                    Generator
+                                </a>
+                            </li>
+                    </ul>
+                </div>
+            </div>
+</div>
---
+++ src/DashboardCommand.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Fishmad\Dashboard;
+
+use File;
+use Illuminate\Console\Command;
+use Symfony\Component\Process\Process;
+
+class DashboardCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'dashboard:install';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Install the Dashboard.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        try {
+            $this->call('migrate');
+        } catch (\Illuminate\Database\QueryException $e) {
+            $this->error($e->getMessage());
+            exit();
+        }
+
+        if (\App::VERSION() >= '5.5') {
+            $this->info("Generating the authentication scaffolding");
+            $this->call('make:auth');
+        }
+
+        $this->info("Publishing the assets");
+        $this->call('vendor:publish', ['--provider' => 'Appzcoder\CrudGenerator\CrudGeneratorServiceProvider', '--force' => true]);
+        $this->call('vendor:publish', ['--provider' => 'Fishmad\Dashboard\DashboardServiceProvider', '--force' => true]);
+
+        $this->info("Dumping the composer autoload");
+        (new Process('composer dump-autoload'))->run();
+
+        $this->info("Migrating the database tables into your application");
+        $this->call('migrate');
+
+        $this->info("Adding the routes");
+
+        $routeFile = app_path('Http/routes.php');
+        if (\App::VERSION() >= '5.5') {
+            $routeFile = base_path('routes/web.php');
+        }a
+
+        $routes =
+            <<<EOD
+Route::get('admin', 'Admin\\AdminController@index');
+Route::get('admin/generator', ['uses' => '\Fishmad\Dashboard\Controllers\ProcessController@getGenerator']);
+Route::post('admin/generator', ['uses' => '\Fishmad\Dashboard\Controllers\ProcessController@postGenerator']);
+EOD;
+
+        File::append($routeFile, "\n" . $routes);
+
+        $this->info("Successfully installed Laravel Admin!");
+    }
+}
